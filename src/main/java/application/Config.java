1b449f1f025f820a3c7dec2d4a664d795d70cb24
wojdzie <wojdzi@outlook.com>
2021-03-29 18:51:34 +0200

    Main class has been added, Config model has been added

---
+++ src/main/java/application/Config.java
@@ -0,0 +1,59 @@
+package application;
+
+public class Config {
+
+    private int width;
+    private int height;
+    private int startEnergy;
+    private int moveEnergy;
+    private int plantEnergy;
+    private double jungleRatio;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getStartEnergy() {
+        return startEnergy;
+    }
+
+    public void setStartEnergy(int startEnergy) {
+        this.startEnergy = startEnergy;
+    }
+
+    public int getMoveEnergy() {
+        return moveEnergy;
+    }
+
+    public void setMoveEnergy(int moveEnergy) {
+        this.moveEnergy = moveEnergy;
+    }
+
+    public int getPlantEnergy() {
+        return plantEnergy;
+    }
+
+    public void setPlantEnergy(int plantEnergy) {
+        this.plantEnergy = plantEnergy;
+    }
+
+    public double getJungleRatio() {
+        return jungleRatio;
+    }
+
+    public void setJungleRatio(double jungleRatio) {
+        this.jungleRatio = jungleRatio;
+    }
+}
---
+++ src/main/java/application/EvolutionaryGeneratorApplication.java
@@ -0,0 +1,8 @@
+package application;
+
+public class EvolutionaryGeneratorApplication {
+
+    public static void main(String...args) {
+        System.out.println("Hello in my evolutionary generator!");
+    }
+}
