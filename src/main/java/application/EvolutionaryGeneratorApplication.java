34400de27c851ab2f7e1ffd43405fd09ed38fd78
wojdzie <wojdzi@outlook.com>
2021-03-31 21:11:20 +0200

    Maven has been added

--- src/main/java/application/EvolutionaryGeneratorApplication.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-public class EvolutionaryGeneratorApplication {
-
-    public static void main(String...args) {
-        System.out.println("Hello in my evolutionary generator!");
-    }
-}
---
+++ src/main/java/com/wojdzie/evolutionary/genrator/EvolutionaryGeneratorApplication.java
@@ -0,0 +1,8 @@
+package com.wojdzie.evolutionary.genrator;
+
+public class EvolutionaryGeneratorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello in my Evolution Generator App!");
+    }
+}
